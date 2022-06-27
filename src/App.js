246e8cabc267bc5c0b8c08dabcb2d31db9732457
ElenaBelaya <makseniya@ukr.net>
2022-06-27 18:48:07 +0300

    added user

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import Profile from './components/Profile';
+import user from './user.json';
+
 export default function App () {
-    console.log('hello');
+    return 
+    <div>
+        <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+console.log(document.querySelector('#root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+//root.render(
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
+ // document.getElementById('root'),
+//);
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
